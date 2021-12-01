3db6ef5467413a60337f326497814ec4eb721df4
zawa1120 <ckz1120@gmail.com>
2021-12-01 23:23:41 +0900

    main

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "google_cloud_scheduler_job" "pubsub_scheduler" {
   name        = "cloud_functions_scheduler"
   region      = var.region
   project     = var.project
-  schedule    = "0 9 * * *"
+  schedule    = "0 12 * * *"
   time_zone   = "Asia/Tokyo"
 
   pubsub_target {
