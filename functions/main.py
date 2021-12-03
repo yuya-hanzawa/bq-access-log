7a2b43f3f6d37e705043d22d86d8e730bf073cb5
zawa1120 <ckz1120@gmail.com>
2021-12-04 00:21:49 +0900

    main

--- functions/main.py
+++ functions/main.py
@@ -4,12 +4,27 @@ import pandas as pd
 from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
 today = datetime.datetime.now()
 log_file = f"{today:access.log-%Y%m%d}"
 
 port = os.environ.get('PORTS')
 username = os.environ.get('USERNAME')
 password = os.environ.get('PASSWORD')
+channel_access_token = os.environ.get("CHANNEL_ACCESS_TOKEN")
+user_id = os.environ.get("USER_ID")
+
+def errir_notification_to_line(channel_access_token, user_id, message):
+    line_bot_api = LineBotApi(channel_access_token)
+
+    try:
+        line_bot_api.push_message(user_id, TextSendMessage(text=message))
+
+    except LineBotApiError as e:
+        raise(e)
 
 def ssh_get_log_file(port, username, password):
     with SSHClient() as ssh:
@@ -25,7 +40,13 @@ def ssh_get_log_file(port, username, password):
             scp.get('/var/log/nginx/' + log_file, '/tmp')
 
 def main(event, context):
-    ssh_get_log_file(port, username, password)
+    try:
+        ssh_get_log_file(port, username, password)
+
+    except Exception as e:
+        errir_notification_to_line(channel_access_token, user_id, 
+                                   message=e)
+        raise(e)
 
     with open('/tmp/'+log_file, errors='ignore') as log:
         df = pd.read_json(log, orient='records', lines=True)
