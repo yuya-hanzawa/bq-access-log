f98b88d1897539b9771df79ddcf58d85131160a8
zawa1120 <ckz1120@gmail.com>
2022-03-05 23:16:14 +0900

    main

--- functions/main.py
+++ functions/main.py
@@ -91,6 +91,27 @@ def main(event, context):
         LINE_notification(f"Error occurred: {traceback.format_exc()}")
         raise(e)
 
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("time", "STRING", mode='NULLABLE', description='標準フォーマットのローカルタイム'),
+            bigquery.SchemaField("remote_host", "STRING", mode='NULLABLE', description='クライアントのIPアドレス'),
+            bigquery.SchemaField("host", "STRING", mode='NULLABLE', description='マッチしたサーバ名もしくはHostヘッダ名、なければリクエスト内のホスト'),
+            bigquery.SchemaField("user", "STRING", mode='NULLABLE', description='クライアントのユーザー名'),
+            bigquery.SchemaField("status", "STRING", mode='NULLABLE', description='レスポンスのHTTPステータスコード'),
+            bigquery.SchemaField("protocol", "STRING", mode='NULLABLE', description='リクエストプロトコル'),
+            bigquery.SchemaField("method", "STRING", mode='NULLABLE', description='リクエストされたHTTPメソッド'),
+            bigquery.SchemaField("path", "STRING", mode='NULLABLE', description='リクエストされたパス'),
+            bigquery.SchemaField("req", "STRING", mode='NULLABLE', description='リクエストURLおよびHTTPプロトコル'),
+            bigquery.SchemaField("size", "STRING", mode='NULLABLE', description='クライアントへの送信バイト数'),
+            bigquery.SchemaField("request_time", "STRING", mode='NULLABLE', description='リクエストの処理時間'),
+            bigquery.SchemaField("upstream_time", "STRING", mode='NULLABLE', description='サーバーがリクエストの処理にかかった時間'),
+            bigquery.SchemaField("user_agent", "STRING", mode='NULLABLE', description='クライアントのブラウザ情報'),
+            bigquery.SchemaField("forwardedfor", "STRING", mode='NULLABLE', description='接続元IPアドレス'),
+            bigquery.SchemaField("forwardedproto", "STRING", mode='NULLABLE', description='HTTP／HTTPS判定'),
+            bigquery.SchemaField("referrer", "STRING", mode='NULLABLE', description='Webページの参照元')
+        ]
+    )
+
     try:
         bq = bigquery.Client(project=project_id)
         dataset = bq.dataset(detaset_id)
@@ -98,9 +119,7 @@ def main(event, context):
         job = bq.load_table_from_dataframe(
             df,
             dataset.table(f'access_log-{day:%Y%m%d}'),
-            job_config=bigquery.LoadJobConfig(
-                schema='schema/schema.json'
-            )
+            job_config=job_config
         )
 
         job.result()
