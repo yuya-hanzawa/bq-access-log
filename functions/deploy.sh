245c4d937809e8176c9d484ca6f0a59deacaadf3
zawa1120 <ckz1120@gmail.com>
2022-06-01 01:23:34 +0900

    main

--- functions/deploy.sh
+++
@@ -1,1 +0,0 @@
-gcloud functions deploy GCF_Access_Log --region us-central1 --entry-point main --runtime python37 --stage-bucket GCF_Access_Log_Bucket --env-vars-file env.yaml --trigger-topic pubsub_topic
